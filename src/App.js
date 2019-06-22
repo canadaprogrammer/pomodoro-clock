881bc9a1623d077545df24aa3f4e0200ffa0efe2
Jin <jinpark1504@gmail.com>
2019-06-21 22:52:42 -0400

    HTML initial setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faArrowUp, faPlay, faPause, faSync } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const iconStyle = {
+    margin: "0 0.5rem"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Pomodoro Clock</h1>
       </header>
+      <main>
+        <div className="length-wrap">
+          <div className="length">
+            <p id="break-label">Break Length</p>
+            <FontAwesomeIcon id="break-decrement" icon={faArrowDown} style={iconStyle}/>
+            <span id="break-length">number</span>
+            <FontAwesomeIcon id="break-increment" icon={faArrowUp} style={iconStyle}/> 
+          </div>
+          <div className="length">
+            <p id="session-label">Session Length</p>
+            <FontAwesomeIcon id="session-decrement" icon={faArrowDown} style={iconStyle}/>
+            <span id="session-length">number</span>
+            <FontAwesomeIcon id="session-increment" icon={faArrowUp} style={iconStyle}/> 
+          </div>
+        </div>
+        <div className="timer-wrap">
+          <p id="timer-label">Session</p>
+          <span id="time-left">25:00</span>
+        </div>
+        <div className="buttons">
+          <FontAwesomeIcon id="start_stop" icon={faPlay} style={iconStyle}/>
+          <FontAwesomeIcon id="reset" icon={faSync} style={iconStyle}/>
+        </div>
+      </main>
     </div>
   );
 }

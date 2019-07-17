df6d3efc13fafbe1af95f7b4e4fdecf8acbd25e7
Jin <jinpark1504@gmail.com>
2019-07-16 20:20:17 -0400

    Updates

---
+++ precache-manifest.7580cde42efbe69e09a55d6f46aec204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b180212d81c987c9c25f2feb01a8e280",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "03241b86260de610942a",
+    "url": "/pomodoro-clock/static/css/main.82b9cced.chunk.css"
+  },
+  {
+    "revision": "0ac5865756b8ac8b9fea",
+    "url": "/pomodoro-clock/static/js/2.871bfc38.chunk.js"
+  },
+  {
+    "revision": "03241b86260de610942a",
+    "url": "/pomodoro-clock/static/js/main.71f900ad.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "582cac299faac1ab8d378a6488ba2ced",
+    "url": "/pomodoro-clock/static/media/Digital_Dismay.582cac29.otf"
+  },
+  {
+    "revision": "b00a7e7853284f5729cfd783d0735c74",
+    "url": "/pomodoro-clock/static/media/iconplaypause1.b00a7e78.png"
+  }
+]);
